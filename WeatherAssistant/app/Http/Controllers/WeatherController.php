a30948da136a46786e969f0479715f621bb66220
Daniel McCarthy <dansblackberry2@hotmail.co.uk>
2024-01-15 04:43:42 +0000

    Lecture 17 - Creating Our Base Code

---
+++ WeatherAssistant/app/Http/Controllers/WeatherController.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class WeatherController extends Controller
+{
+    public function index()
+    {
+        return view('weather.home');
+    }
+}
